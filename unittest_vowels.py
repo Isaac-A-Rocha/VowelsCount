d1da3dd0ece540106176355b2c3596d168a557a4
Leandro <kiyohiroleo@gmail.com>
2026-09-03 16:00:08 -0300

    Três testes; string vazia, número, sem vogais.

--- unittest_vowels.py
+++ unittest_vowels.py
@@ -3,10 +3,17 @@ from count_utils import count_vowels
 
 class CountVowels(unittest.TestCase):
 
-    def test_1(self):
-        pass
-    def test_1(self):
-        pass
+    #Teste com uma String vazia.
+    def testEmptyString(self):
+        self.assertEqual(count_vowels(""), 0)
+    #Teste usando um valor numérico como entrada, caso a entrada cause um TypeError, está correto.
+    def testNumber(self):
+        with self.assertRaises(TypeError):
+            count_vowels(0)
+    #Teste se não há vogais.
+    def testNoVowels(self):
+        self.assertEqual(count_vowels("wxyz"), 0)
 
 if __name__ == '__main__':
     unittest.main()
+#ou: 'python3 -m unittest unittest_vowels.py -v' no terminal para exibir cada teste separadamente.
